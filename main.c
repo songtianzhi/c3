4c908e3545ecfceac1ecb28430adba245ff9a9ba
小智 <123123@qq.com>
2019-01-17 08:45:24 +0800

    构建基本框架与增加添加敏感字符

--- main.c
+++ main.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
-
+int index = 0;
+char arr[1024];
 int main(){
+    int code = 0;
+    printf("进入敏感字符系统\n");
 
-    printf("hello world!");
+    
+    while(1){
+        printf("1--添加一个敏感字符\n");
+        printf("2--删除一个敏感字符\n");
+        printf("3--显示所有敏感字符\n");
+        printf("4--替换敏感字符\n");
+        printf("5--退出系统\n");
 
-    return 0;
+        scanf("%d",&code);
+        printf("请输入相对应的功能编号：\n");
 
+        
+        if (code == 1) {
+            char a ;
+            printf("请输入要添加的敏感字符\n");
+
+            scanf("%c",&a);
+            scanf("%c",&a);
+
+            arr[index] = a;
+            index++;
+            printf("添加成功，点击回车继续\n");
+
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+            
+        }
+        if (code == 2) {
+            
+        }
+        if (code == 3) {
+            
+        }
+        if (code == 4) {
+            
+        }
+        if (code == 5) {
+            printf("退出系统\n");
+            break;
+        }
+    }
+    
 }
